750da345e7e504598863818293a01f28e3781e50
Patrick Braje <patrick.braje@gmail.com>
2022-02-22 09:13:42 +0100

    Add CommandLine Unit Test

---
+++ test/de/dhbw/kontoverwaltung/terminal/CommandResultTest.java
@@ -0,0 +1,40 @@
+package de.dhbw.kontoverwaltung.terminal;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class CommandResultTest {
+
+	@Test
+	void testSuccessData() {
+		CommandResult target = CommandResult.success("test ok");
+
+		assertThat(target.isSuccessful(), is(true));
+		assertThat(target.getAdditionalInfo(), is("test ok"));
+	}
+
+	@Test
+	void testErrorData() {
+		CommandResult target = CommandResult.error("test error");
+
+		assertThat(target.isSuccessful(), is(false));
+		assertThat(target.getAdditionalInfo(), is("test error"));
+	}
+
+	@Test
+	void testSuccessToString() {
+		CommandResult target = CommandResult.success("test ok");
+
+		assertThat(target.toString(), is("OK: test ok"));
+	}
+
+	@Test
+	void testErrorToString() {
+		CommandResult target = CommandResult.error("test error");
+
+		assertThat(target.toString(), is("ERROR: test error"));
+	}
+
+}
