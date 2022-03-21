b9508b6d669ff33a1c3acdc0b14ff2e7fdd66f4d
Patrick Braje <PatrickB2109@mailbox.org>
2022-03-21 15:34:01 +0100

    Adjust BankEvents

--- src/de/dhbw/kontoverwaltung/events/bank/BankEvents.java
+++ src/de/dhbw/kontoverwaltung/events/bank/BankEvents.java
@@ -3,8 +3,12 @@ package de.dhbw.kontoverwaltung.events.bank;
 import de.dhbw.kontoverwaltung.terminal.command.CommandResult;
 
 public interface BankEvents {
+	
+	public CommandResult getBank(String name);
 
 	public CommandResult createBank(String name);
+	
+	public CommandResult editBank(String oldName, String newName);
 
 	public CommandResult deleteBank(String name);
 
--- src/de/dhbw/kontoverwaltung/events/bank/BankEventsImpl.java
+++ src/de/dhbw/kontoverwaltung/events/bank/BankEventsImpl.java
@@ -5,13 +5,27 @@ import de.dhbw.kontoverwaltung.terminal.command.CommandResult;
 public class BankEventsImpl implements BankEvents {
 
 	@Override
+	public CommandResult getBank(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
 	public CommandResult createBank(String name) {
-		return CommandResult.error("not implemented yet");
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CommandResult editBank(String oldName, String newName) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
 	public CommandResult deleteBank(String name) {
-		return CommandResult.error("not implemented yet");
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
