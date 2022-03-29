02b3cff52823bed01ed466362bb36a174099fe37
Simon <59486823+simon2022@users.noreply.github.com>
2022-03-29 12:19:57 +0200

    refactoring splitted command test

--- test/de/dhbw/kontoverwaltung/terminal/SplittedCommandTest.java
+++ test/de/dhbw/kontoverwaltung/terminal/SplittedCommandTest.java
@@ -9,22 +9,40 @@ import org.junit.jupiter.api.Test;
 
 class SplittedCommandTest {
 
+	private SplittedCommand splittedCommand = new SplittedCommand("kunde delete 122 now", " ");
+
 	@Test
-	void test() {
-		SplittedCommand splittedCommand = new SplittedCommand("kunde delete 122 now", " ");
-		assertThat(splittedCommand.getStringUppercaseAt(0), is("KUNDE"));
+	void testStringAt() {
 		assertThat(splittedCommand.getStringAt(1), is("delete"));
-		assertThat(splittedCommand.getStringAt(3), is("now"));
-
 		assertThat(splittedCommand.getStringAt(2), is("122"));
-		assertThat(splittedCommand.getIntAt(2), is(122));
+		assertThat(splittedCommand.getStringAt(3), is("now"));
+	}
 
+	@Test
+	void testUppercase() {
+		assertThat(splittedCommand.getStringUppercaseAt(0), is("KUNDE"));
+	}
+
+	@Test
+	void testIntAt() {
+		assertThat(splittedCommand.getIntAt(2), is(122));
+	}
+
+	@Test
+	void testNumberFormatException() {
 		assertThrows(NumberFormatException.class, () -> {
 			splittedCommand.getIntAt(1);
 		});
+	}
+
+	@Test
+	void testNull() {
 		assertNull(splittedCommand.getStringAt(4));
 		assertNull(splittedCommand.getStringUppercaseAt(4));
+	}
 
+	@Test
+	void testCommandUpToPos() {
 		assertThat(splittedCommand.getCommandUpToPos(3), is("kunde delete 122"));
 	}
 
